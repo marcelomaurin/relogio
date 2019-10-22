12ef09a3c781928a949f3fe737b5c4ae713864c3
marcelomaurinmartins <marcelomaurinmartins@gmail.com>
2019-10-22 20:43:58 -0300

    atualizacao necessaria

---
+++ arduino/relogio3/relogio3.ino
@@ -0,0 +1,65 @@
+
+/*
+Library Originally added by David A Mellis
+Library Modified by Limor Fried
+Example added by Tom Igoe
+Modified by Tom Igoe
+This example is in the public domain. http://www.arduino.cc.en/Tutorial/LiquidCrystal
+Contrast modification by Ahmed Murtaza Qureshi (www.engineeringlearning.blogspot.com)
+*/
+#include <LiquidCrystal.h>
+char ch;
+int Contrast=15;
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() 
+{
+  Serial.begin(9600);
+  Serial.println("LCD test with PWM contrast adjustment");
+  pinMode(13,OUTPUT);
+  analogWrite(6,Contrast);
+  // set up the LCD's number of columns and rows: 
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("LCD test!!");
+}
+
+void loop() 
+{
+   digitalWrite(13,LOW);
+   delay(1000);
+   digitalWrite(13,HIGH);
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print(millis()/1000);
+}
+
+void serialEvent()
+{
+     if (Serial.available())
+  {
+    ch= Serial.read();
+    if(ch=='A' && Contrast<255)
+    {
+      Contrast=Contrast+1;
+    }
+    if(ch=='B' && Contrast>0)
+    {
+      Contrast=Contrast-1;
+    }
+        if(ch=='N')
+    {
+      analogWrite(9,28836);
+    }
+       if(ch=='F')
+    {
+      analogWrite(9,0);
+    }
+    analogWrite(6,Contrast);
+    Serial.println("Current contrast");
+    Serial.println(Contrast);
+  }
+}
